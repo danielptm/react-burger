1bc2c0a717ff6cc48f9ce3eb6fcc200ffe85bd37
Daniel <danielptm@me.com>
2018-02-14 19:46:34 +0100

    Got modal working lecture 122

--- src/containers/BurderBuilder/BurgerBuilder.js
+++ src/containers/BurderBuilder/BurgerBuilder.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
     meat: 1.3,
     bacon: 0.7
-
 }
 
 class BurgerBuilder extends Component{
@@ -84,6 +85,9 @@ class BurgerBuilder extends Component{
         }
         return (
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger
                     ingredients={this.state.ingredients}
                 />
@@ -94,7 +98,6 @@ class BurgerBuilder extends Component{
                     price={this.state.totalPrice}
                     purchaseable={this.state.purchasable}
                 />
-
             </Aux>
         );
     }
